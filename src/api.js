53aa0171ef3a62f58b9a07a7e589bac2d6603e38
Orhan Toy <orhantoy@github.com>
2024-04-30 22:48:30 +0000

    Prefix envs for client-side

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export function apiPath(route) {
-    const host = process.env.API_HOST || 'http://localhost';
-    const port = process.env.API_PORT || 4051;
-    const basePath = process.env.BASE_PATH || '/api';
-  
-    return `${host}:${port}${basePath}${route}`; 
+    const host = process.env.REACT_APP_API_HOST || 'http://localhost';
+    const port = process.env.REACT_APP_API_PORT || 4051;
+    const basePath = process.env.REACT_APP_BASE_PATH || '/api';
+
+    return `${host}:${port}${basePath}${route}`;
 }
