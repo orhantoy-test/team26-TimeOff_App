e3fdc18f4883b49fb319842272ca4d5a69094f77
Orhan Toy <orhantoy@github.com>
2024-04-30 22:43:19 +0000

    Serve static files

--- src/backend/app.js
+++ src/backend/app.js
@@ -12,6 +12,7 @@ const cors = require("cors");
 app.use(express.static(buildPath));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 // app.use(cors({
